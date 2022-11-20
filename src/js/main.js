4f54a7b4d02f1fb84e3f7cbf9b45b70f6d019bf9
euxzy <rijal.m12l@gmail.com>
2022-11-20 17:22:32 +0700

    change header background on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,21 @@ menu.addEventListener('click', () => {
   navMenu.classList.toggle('hide-nav')
   navMenu.classList.toggle('show-nav')
 })
+
+// change background navbar on scroll
+const header = getEl('header')
+const hero = getEl('#hero')
+const btnNav = getEl('#btn-nav')
+const btnLogin = getEl('#btn-login')
+
+window.onscroll = () => {
+  if (window.pageYOffset > hero.offsetTop + 250) {
+    header.classList.remove('md:bg-transparent')
+    btnNav.classList.remove('md:text-black')
+    btnLogin.classList.remove('md:border-black')
+  } else {
+    header.classList.add('md:bg-transparent')
+    btnNav.classList.add('md:text-black')
+    btnLogin.classList.add('md:border-black')
+  }
+}
