f9fe9e8e8d97d06c088bd426396196b35ff8897b
euxzy <rijal.m12l@gmail.com>
2022-11-20 22:47:08 +0700

    add hover transition on navbar

--- src/js/main.js
+++ src/js/main.js
@@ -14,16 +14,19 @@ menu.addEventListener('click', () => {
 const header = getEl('header')
 const hero = getEl('#hero')
 const btnNav = getEl('#btn-nav')
+const btnSignUp = getEl('#btn-signup')
 const btnLogin = getEl('#btn-login')
 
 window.onscroll = () => {
   if (window.pageYOffset > hero.offsetTop + 150) {
     header.classList.remove('md:bg-transparent')
     btnNav.classList.remove('md:text-black')
+    btnSignUp.classList.remove('md:hover:border-black')
     btnLogin.classList.remove('md:border-black')
   } else {
     header.classList.add('md:bg-transparent')
     btnNav.classList.add('md:text-black')
+    btnSignUp.classList.add('md:hover:border-black')
     btnLogin.classList.add('md:border-black')
   }
 }
